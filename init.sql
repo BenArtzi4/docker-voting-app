075de1093d71ddf4983fc7f03d553a3a09248660
BenArtzi4 <benartzi4@gmail.com>
2024-08-10 16:22:42 +0300

    add init sql file

---
+++ init.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE IF NOT EXISTS votes_db;
+
+USE votes_db;
+
+CREATE TABLE IF NOT EXISTS votes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    choice ENUM('android', 'apple') NOT NULL
+);
